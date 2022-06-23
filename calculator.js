359739641de43fe138d2ce50eb60e25683364ce3
miykelJoe <99487006+miykelJoe@users.noreply.github.com>
2022-06-23 11:57:38 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+let chooseOperator = prompt('Choose the operation you want to perform * / + - %');
+
+let firstNumber = prompt('Enter first figure');
+let secondNumber = prompt('Enter second figure... ' + firstNumber + ' ' + chooseOperator);
+
+let multiply = firstNumber * secondNumber;
+let divide = firstNumber / secondNumber;
+let addition = (Number (firstNumber)) + (Number (secondNumber));
+let subtraction = firstNumber - secondNumber;
+let modulus = firstNumber % secondNumber;
+
+
+if (chooseOperator === '*'){
+    alert('Your answer is ' + firstNumber +  ' * ' + secondNumber + ' = ' + multiply);
+}else if (chooseOperator === '/'){
+    alert('Your answer is ' + firstNumber +  ' / ' + secondNumber + ' = ' + divide);
+}else if (chooseOperator === '+'){
+    alert('Your answer is ' + firstNumber +  ' + ' + secondNumber + ' = ' + addition);
+}else if (chooseOperator === '-'){
+    alert('Your answer is ' + firstNumber +  ' - ' + secondNumber + ' = ' + subtraction);
+}else if (chooseOperator === '%'){
+    alert('Your answer is ' + firstNumber +  ' % ' + secondNumber + ' = ' + modulus);
+}else {
+    alert('The operator you chose is not a valid operator.');
+}
